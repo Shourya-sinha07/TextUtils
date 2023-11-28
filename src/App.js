f5be0148277ddf25fe7987d4dd6e5ed5db4c77e8
Shourya-sinha07 <shouryasinha330@gmail.com>
2023-11-28 11:46:35 +0530

    Adding Routinh

--- src/App.js
+++ src/App.js
@@ -1,68 +1,52 @@
-//import logo from './logo.svg';
-import { useState } from 'react';
-import './App.css';
-// import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-import React from "react";
-// now we stop routing and remove about pages
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   Routes,
-//   Link
-// } from "react-router-dom";
-// let nam="harry";
 function App() {
-  const [mode, setmode] =useState('light')  //weather dark mode is is enable or not
-  const [alert, setalert ]=useState(null)
-  const showAlert=(message, type)=>{
-    setalert ({
-      msg:message,
-      type:type
-    })
+  const [mode, setmode] = useState("light");
+  const [alert, setalert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setalert({
+      msg: message,
+      type: type,
+    });
     setTimeout(() => {
-      setalert(null)
+      setalert(null);
     }, 2000);
-  }
-  const togglemode=()=>{
-    if(mode ==='light'){
-      setmode('dark')
-      document.body.style.backgroundColor ='#042743'
-      showAlert( "Dark mode has been enabled","success")
-    }else{
-      setmode('light')
-      document.body.style.backgroundColor ='white'
-      showAlert("light mode has been enabled","success" )
+  };
 
-
+  const togglemode = () => {
+    if (mode === "light") {
+      setmode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setmode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
+
   return (
-   <>
-{/* <Router> */}
-<Navbar title="TextUtils" mode={mode} togglemode={togglemode}/>
-<Alert alert={alert}></Alert>
-<div className="container">
-        {/* <Routes> */}
-          {/* <Route path="/about">
-            <About />
-          </Route> */}
-          {/* Always use exact before the path because react always use partial routing which means if one path is /user which render component 1 and path /user/home which render component 2 and if we not use exact then it we try to follow path /user/home then also /user component 1 is render because react always use partial routing  */}
-          {/* <Route exact path="/about" element={<About />} />    */}
-          {/* <Route  path="/"> */}
-          <TextForm heading="Enter the text below to analyze" showAlert={showAlert} mode={mode}/>
-            {/* <Home /> */}
-          {/* </Route> */}
-          {/* <Route exact path="/" element={<TextForm heading="Enter the text below to analyze" showAlert={showAlert} mode={mode}/>} /> */}
-        {/* </Routes> */}
-</div>
-{/* </Router> */}
-   </>
-);
+    <>
+      <Router>
+        <Navbar title="TextUtils" mode={mode} togglemode={togglemode} />
+        <Alert alert={alert} />
+        {/* <About></About> */}
+        <div className="container">
+          <Routes>
+            <Route path="/" element={<TextForm heading="Enter the text below to analyze" showAlert={showAlert} mode={mode} />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
 }
 
 export default App;
--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,4 @@
-import { useCallback, useState } from "react";
+import {  useState } from "react";
 // import React {useState} from "react";
 
 export default function About() {
